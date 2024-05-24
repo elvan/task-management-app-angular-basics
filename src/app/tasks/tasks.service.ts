5c9c0a17d945161d8b2745c0ed30dc07916ffc98
Elvan Hidayat <elvanhd@gmail.com>
2024-05-25 05:18:49 +0700

    Persist tasks to local storage in TasksService
    - Load tasks from local storage in TasksService constructor
    - Save tasks to local storage after adding a task
    - Save tasks to local storage after removing a task
    - Implement private saveTasks method for local storage update

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -30,6 +30,14 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -42,9 +50,15 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.date,
     });
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
