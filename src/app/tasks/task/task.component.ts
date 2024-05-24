173072ce420eac246c2a2f71e432b9321db586d6
Elvan Hidayat <elvanhd@gmail.com>
2024-05-25 04:45:17 +0700

    Create reusable card component for consistent styling
    - Add CardComponent for reusable card UI
    - Update TaskComponent to use CardComponent for task display
    - Apply date pipe to format due dates in TaskComponent
    - Modify UserComponent to use CardComponent for user display
    - Remove redundant styles from UserComponent CSS

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,13 +1,15 @@
+import { DatePipe } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+import { CardComponent } from '../../shared/card/card.component';
 import { type Task } from './task.model';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
+  imports: [CardComponent, DatePipe],
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+import { CardComponent } from '../shared/card/card.component';
 import { type User } from './user.model';
 
 @Component({
@@ -7,6 +8,7 @@ import { type User } from './user.model';
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
+  imports: [CardComponent],
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
