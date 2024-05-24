e6d8de01f46a075431609de84a326551f1b95b6c
Elvan Hidayat <elvanhd@gmail.com>
2024-05-24 16:25:43 +0700

    Add event emission for user selection in User component
    - Add id input to User component for unique identification
    - Implement EventEmitter in User component for selection event
    - Update app.component.html to handle user selection event
    - Add onSelectUser method in AppComponent to log selected user id

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,8 @@ import { UserComponent } from './user/user.component';
 })
 export class AppComponent {
   users = DUMMY_USERS;
+
+  onSelectUser(id: string) {
+    console.log('Selected user with id ' + id);
+  }
 }
