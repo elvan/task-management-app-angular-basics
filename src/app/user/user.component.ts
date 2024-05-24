f2bd2d7963d865fcb20881d45ba0214a4d66dcbf
Elvan Hidayat <elvanhd@gmail.com>
2024-05-24 17:38:24 +0700

    Add task management functionality and refactor code
    - Create TaskComponent for displaying individual tasks
    - Define Task model for task structure
    - Update TasksComponent to filter and display tasks per user
    - Modify AppComponent to pass userId to TasksComponent
    - Refactor User component to use a shared User model

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,16 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-// type User = {
-//   id: string;
-//   avatar: string;
-//   name: string;
-// }
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
