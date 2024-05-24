144cc8061a81fc5f0d14158e14ad1807193a6ce2
Elvan Hidayat <elvanhd@gmail.com>
2024-05-24 16:35:50 +0700

    Add Tasks component to display selected user tasks
    - Create Tasks component with input for user name
    - Update app.component.html to include Tasks component
    - Implement selectedUser getter in AppComponent
    - Update onSelectUser method to set selectedUserId
    - Import TasksComponent in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { DUMMY_USERS } from './dummy-users';
 import { HeaderComponent } from './header/header.component';
+import { TasksComponent } from './tasks/tasks.component';
 import { UserComponent } from './user/user.component';
 
 @Component({
@@ -9,12 +10,17 @@ import { UserComponent } from './user/user.component';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
 
   onSelectUser(id: string) {
-    console.log('Selected user with id ' + id);
+    this.selectedUserId = id;
   }
 }
