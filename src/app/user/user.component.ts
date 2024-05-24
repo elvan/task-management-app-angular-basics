3e254a87493546a64503cbab18a536492cf06a07
Elvan Hidayat <elvanhd@gmail.com>
2024-05-24 16:55:34 +0700

    Refactor User component to use single user input
    - Update User component to accept a single user object as input
    - Modify app.component.html to pass user object to User component
    - Adjust template bindings in user.component.html for user object
    - Simplify onSelectUser method to emit user id from user object
    - Implement User interface to define user object structure

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,17 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,16 +19,14 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
