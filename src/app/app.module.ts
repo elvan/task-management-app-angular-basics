8d2778cf85098aeb74b54c28f554482679b809ed
Elvan Hidayat <elvanhd@gmail.com>
2024-05-25 12:13:36 +0700

    Migrate to NgModule and update component imports
    - Create AppModule to manage application modules and components
    - Move component imports and declarations to AppModule
    - Remove standalone property from all components
    - Update main.ts to bootstrap AppModule instead of AppComponent
    - Add FormsModule to AppModule imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { CardComponent } from './shared/card/card.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { TaskComponent } from './tasks/task/task.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { UserComponent } from './user/user.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    CardComponent,
+    TasksComponent,
+    TaskComponent,
+    NewTaskComponent,
+  ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
+  imports: [BrowserModule, FormsModule],
 })
 export class AppModule {}
--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,16 +1,12 @@
-import { DatePipe } from '@angular/common';
 import { Component, Input, inject } from '@angular/core';
 
-import { CardComponent } from '../../shared/card/card.component';
 import { TasksService } from '../tasks.service';
 import { type Task } from './task.model';
 
 @Component({
   selector: 'app-task',
-  standalone: true,
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
-  imports: [CardComponent, DatePipe],
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
